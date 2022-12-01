1f0f2dcd13405297c6598c202702b42d50156646
Sean Bickle <sean.a.bickle@gmail.com>
2022-12-01 22:40:00 +0000

    task: Integrate with frontend
    Frontend tiled using the new css property aspect-ratio. Uncertain of the
    support for this, but it's a much simpler tiling mechanism than the
    `padding-top` hack I used for griddle.
    
    Implemented `Letter` class to abstract some of the Javascript logic
    from the HTML elements. Each `Letter` object maps to one of the buttons
    on the bottom row to avoid having to interact with the HTML directly
    from within the Javascript.
    
    The user selection is rendered at the top by simply selecting the next
    "unselected" element in the row. Eventually, the user will be able to
    clear the selection or submit the selection. If the submission is a
    word, it will get rendered in the `output-container` div.
    
    The output-container div currently goes behind the buttons at the
    bottom. Some padding will be required to ensure the last words in the
    grid are still visible.

--- script.js
+++ script.js
@@ -3,19 +3,34 @@
 CHARS = "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAARRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNSSSSSSSSSSSSSSSSSSSSSSSSSSSSSLLLLLLLLLLLLLLLLLLLLLLLLLLLLCCCCCCCCCCCCCCCCCCCCCCCUUUUUUUUUUUUUUUUUUUDDDDDDDDDDDDDDDDDPPPPPPPPPPPPPPPPMMMMMMMMMMMMMMMHHHHHHHHHHHHHHHGGGGGGGGGGGGGBBBBBBBBBBBFFFFFFFFFYYYYYYYYYWWWWWWWKKKKKKVVVVVXXZZJJQQ"
 // Number of chars to play with
 SELECTION_COUNT = 10
+// The chars which the user can use to input a word
+INPUT_CHARS = []
+// Selection
+SELECTION = []
 
 function get_random_char(){
     // Get a random char from the CHARS distribution
     return CHARS[Math.floor(Math.random() * CHARS.length)]
 }
 
-function select_chars(num_chars){
-    // Select a range of chars for the game
-    chars = []
-    for(i = 0; i < num_chars; i++){
-        chars.push(get_random_char())
+class Letter{
+    constructor(index){
+        this.index = index
+        this.char = get_random_char()
+        this.el = document.getElementById('letter-btn-' + index)
+        this.selected = false
+
+        this.render_char()
     }
-    return chars
+
+    render_char(){
+        this.el.innerText = this.char
+    }
+
+    select(){
+        this.selected = true
+        this.el.classList.add('letter-btn-selected')
+    }
 }
 
 function is_word(word){
@@ -23,4 +38,25 @@ function is_word(word){
     return WORDLIST.includes(word)
 }
 
+function generate_game_data(num_chars){
+    // Generate a list of chars for game and set game data
+    chars = []
+    for(i = 0; i < num_chars; i++) chars.push(new Letter(i))
+    return chars
+}
 
+// UI buttons
+function select_letter_btn(tile_index){
+    tile = INPUT_CHARS[tile_index]
+    // Don't allow tiles to be re-selected
+    if(tile.selected) return
+    tile.select()
+    SELECTION.push(tile.char)
+    selection_tile = document.getElementsByClassName('letter-selection unselected')[0]
+    selection_tile.classList.remove('unselected')
+    selection_tile.innerText = tile.char
+}
+
+window.onload = function(){
+    INPUT_CHARS = generate_game_data(SELECTION_COUNT)
+}
